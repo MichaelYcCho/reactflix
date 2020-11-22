a26c6cfc12fff9025f9d60015cc33bbf4e2e37e7
Youngchan <michael_cho77@naver.com>
2020-11-22 20:38:14 +0900

    Poster Component part Two

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -66,7 +66,7 @@ const SearchPresenter = ({
                                         imageUrl={show.poster_path}
                                         title={show.original_name}
                                         rating={show.vote_average}
-                                        year={show.first_air_date.substring(0, 4)}
+                                        year={show.release_date && show.first_air_date.substring(0, 4)}
                                     />
                                 ))}
                             </Section>
